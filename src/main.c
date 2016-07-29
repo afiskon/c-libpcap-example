590cba6649173eb0acf7103999d61f38a05fbcea
Aleksander Alekseev <afiskon@gmail.com>
2016-07-29 11:33:58 +0300

    Source code moved from sandbox repository

---
+++ src/main.c
@@ -0,0 +1,173 @@
+#include <pcap/pcap.h>
+#include <net/ethernet.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// TODO: support text only output, use argp
+// TODO: test in IPv6 network
+// TODO: list all available interfaces
+
+#define UNUSED(x) ((void)(x))
+
+#define IP_HEADER_PROTOCOL_TCP 6
+#define IP_HEADER_PROTOCOL_UDP 17
+
+#define PRINT_BYTES_PER_LINE 16
+
+static void
+print_data_hex(const uint8_t* data, int size)
+{
+    UNUSED(data);
+    UNUSED(size);
+
+    int offset = 0;
+    int nlines = size / PRINT_BYTES_PER_LINE;
+    if(nlines * PRINT_BYTES_PER_LINE < size)
+        nlines++;
+
+    printf("        ");
+
+    for(int i = 0; i < PRINT_BYTES_PER_LINE; i++)
+    {
+        printf("%02X ", i);
+    }
+
+    printf("\n\n");
+
+    for(int line = 0; line < nlines; line++)
+    {
+        printf("%04X    ", offset);
+        for(int j = 0; j < PRINT_BYTES_PER_LINE; j++)
+        {
+            if(offset + j >= size)
+                printf("   ");
+            else
+                printf("%02X ", data[offset + j]);
+        }
+
+        printf("   ");
+
+        for(int j = 0; j < PRINT_BYTES_PER_LINE; j++)
+        {
+            if(offset + j >= size)
+                printf(" ");
+            else if(data[offset + j] > 31 && data[offset + j] < 128)
+                printf("%c", data[offset + j]);
+            else
+                printf(".");
+        }
+
+        offset += PRINT_BYTES_PER_LINE;
+        printf("\n");
+    }
+}
+
+static void
+handle_packet(uint8_t* user, const struct pcap_pkthdr *hdr,
+                const uint8_t* bytes)
+{
+    UNUSED(user);
+
+    // struct ethhdr* ethernet_header = (struct ethhdr *)bytes;
+    struct iphdr* ip_header = (struct iphdr*)(bytes + sizeof(struct ethhdr));
+    struct sockaddr_in  source = {0},
+                        dest = {0};
+
+    source.sin_addr.s_addr = ip_header->saddr;
+    dest.sin_addr.s_addr = ip_header->daddr;
+    char source_ip[128];
+    char dest_ip[128];
+    strncpy(source_ip, inet_ntoa(source.sin_addr), sizeof(source_ip));
+    strncpy(dest_ip, inet_ntoa(dest.sin_addr), sizeof(dest_ip));
+
+    int source_port = 0;
+    int dest_port = 0;
+    int data_size = 0;
+    int ip_header_size = ip_header->ihl * 4;
+    char* next_header = (char*)ip_header + ip_header_size;
+
+    if(ip_header->protocol == IP_HEADER_PROTOCOL_TCP)
+    {
+        struct tcphdr* tcp_header = (struct tcphdr*)next_header;
+        source_port = ntohs(tcp_header->source);
+        dest_port = ntohs(tcp_header->dest);
+        int tcp_header_size = tcp_header->doff * 4;
+        data_size = hdr->len - sizeof(struct ethhdr) -
+                        ip_header_size - tcp_header_size;
+    }
+    else if(ip_header->protocol == IP_HEADER_PROTOCOL_UDP)
+    {
+        struct udphdr* udp_header = (struct udphdr*)next_header;
+        source_port = ntohs(udp_header->source);
+        dest_port = ntohs(udp_header->dest);
+        data_size = hdr->len - sizeof(struct ethhdr) -
+                        ip_header_size - sizeof(struct udphdr);
+    }
+
+    printf("\n%s:%d -> %s:%d, %d (0x%x) bytes\n\n",
+        source_ip, source_port, dest_ip, dest_port, data_size, data_size);
+
+    if(data_size > 0)
+    {
+        int headers_size = hdr->len - data_size;
+        print_data_hex(bytes + headers_size, data_size);
+    }
+}
+
+int
+main(int argc, char* argv[])
+{
+    int res;
+
+    if(argc <= 2)
+    {
+        printf("Usage: %s device filter\n", argv[0]);
+        printf("Example: %s eth0 'udp src or dst port 53'\n", argv[0]);
+        printf("%s\n", pcap_lib_version());
+        return 1;
+    }
+
+    const char* device = argv[1];
+    const char* filter = argv[2];
+    char errbuf[PCAP_ERRBUF_SIZE];
+
+    pcap_t* pcap = pcap_open_live(device, 65536, 1, 0, errbuf);
+    if(pcap == NULL)
+    {
+        fprintf(stderr, "pcap_open_live failed: %s\n", errbuf);
+        return 1;
+    }
+
+    struct bpf_program filterprog;
+    res = pcap_compile(pcap, &filterprog, filter, 0, PCAP_NETMASK_UNKNOWN);
+    if(res != 0)
+    {
+        fprintf(stderr, "pcap_compile failed: %s\n", pcap_geterr(pcap));
+        pcap_close(pcap);
+        return 1;
+    }
+
+    res = pcap_setfilter(pcap, &filterprog);
+    if(res != 0)
+    {
+        fprintf(stderr, "pcap_setfilter failed: %s\n", pcap_geterr(pcap));
+        pcap_close(pcap);
+        return 1;
+    }
+
+    printf("Listening %s, filter: %s...\n", device, filter);
+
+    res = pcap_loop(pcap, -1, handle_packet, NULL);
+    printf("pcap_loop returned %d\n", res);
+
+    pcap_close(pcap);
+    return 0;
+}
+
